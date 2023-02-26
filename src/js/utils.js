79e69e392bf46567aaa10be50bf3c2e46dfd535e
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-26 18:47:59 +0200

    Update utils.js

--- src/js/utils.js
+++ src/js/utils.js
@@ -5,10 +5,10 @@ export default {
   debounce,
 
   error(msg, timeout = 1500) {
-    Notify.failure(msg, { timeout });
+    Notify.failure(msg, { timeout, showOnlyTheLastOne: true });
   },
 
   info(msg, timeout = 1500) {
-    Notify.info(msg, { timeout });
+    Notify.info(msg, { timeout, showOnlyTheLastOne: true });
   },
 };
